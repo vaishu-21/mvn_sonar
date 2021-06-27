a3d34faaf6f7f15667d9618fc16a86dd2bf1f35c
salagarsprabu <37106724+salagarsprabu@users.noreply.github.com>
2021-06-27 22:51:13 +0530

    updated vpc and instance names

--- main.tf
+++ main.tf
@@ -17,10 +17,10 @@ provider "google" {
 }
 
 resource "google_compute_network" "vpc_network" {
-  name = "terraform-network1"
+  name = "terraform-network2"
 }
 resource "google_compute_instance" "vm_instance" {
-  name         = "terraform-instance1"
+  name         = "terraform-instance2"
   machine_type = "f1-micro"
 
   boot_disk {
