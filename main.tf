f98647e551b072c488021cb29166af685fbd086c
salagarsprabu <37106724+salagarsprabu@users.noreply.github.com>
2021-06-27 22:01:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ provider "google" {
 }
 
 resource "google_compute_network" "vpc_network" {
-  name = "terraform-network"
+  name = "terraform-network1"
 }
 resource "google_compute_instance" "vm_instance" {
   name         = "terraform-instance1"
