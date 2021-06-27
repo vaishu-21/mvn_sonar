6a269b91291501bc88c2d797b45e0c852f4bb3f8
salagarsprabu <37106724+salagarsprabu@users.noreply.github.com>
2021-06-27 21:35:26 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,36 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "3.5.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file("terraform-project-318114-b9c816aa24cd.json")
+
+  project = "terraform-project-318114"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance1"
+  machine_type = "f1-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {
+    }
+  }
+}
