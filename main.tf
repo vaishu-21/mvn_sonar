514a2655b44701344b2440b8455497dfec7788fb
salagarsprabu <37106724+salagarsprabu@users.noreply.github.com>
2021-06-28 13:27:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -41,7 +41,8 @@ resource "google_compute_instance" "vm_instance" {
 
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-9"
+     ## image = "debian-cloud/debian-9"
+      image = "ubuntu-2004-focal-v20210510"
     }
   }
 
