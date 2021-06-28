788021d42c41cfc6a468331a94e5dfd996030a0e
salagarsprabu <37106724+salagarsprabu@users.noreply.github.com>
2021-06-28 13:13:46 +0530

    add firewall rules

--- main.tf
+++ main.tf
@@ -16,6 +16,22 @@ provider "google" {
   zone    = "us-central1-c"
 }
 
+resource "google_compute_firewall" "default" {
+  name    = "test-firewall"
+  network = google_compute_network.default.name
+
+  allow {
+    protocol = "icmp"
+  }
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22","80", "8080", "1000-2000"]
+  }
+
+  source_tags = ["web"]
+}
+
 resource "google_compute_network" "vpc_network" {
   name = "terraform-network"
 }
